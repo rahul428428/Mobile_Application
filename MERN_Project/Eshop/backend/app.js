3a69874a0f520e38b71f3ef50759b7979ad50dd2
rahul428428 <1996meenarahul@gmail.com>
2021-04-27 16:37:52 +0530

    categories model and scheme

--- MERN_Project/Eshop/backend/app.js
+++ MERN_Project/Eshop/backend/app.js
@@ -4,7 +4,11 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const api = process.env.API_URL
+
+app.use(cors()) // to use cors
+app.options('*', cors()) // HTTP OPTIONS method requests permitted communication options for a given URL or server. A client can specify a URL with this method, or an asterisk (*) to refer to the entire server.
 
 //middleware
 app.use(express.json()) //for parsing the Json
@@ -16,10 +20,10 @@ const productRoutes = require('./routes/products') // Coming from  products.js  
 const userRoutes = require('./routes/users') // Coming from  users.js  folder and routes
 const orderRoutes = require('./routes/orders') // Coming from  orders.js  folder and routes
 
-app.use(`${api}/products`, categoriesRoutes)
+app.use(`${api}/categories`, categoriesRoutes)
 app.use(`${api}/products`, productRoutes)
-app.use(`${api}/products`, userRoutes)
-app.use(`${api}/products`, orderRoutes)
+app.use(`${api}/users`, userRoutes)
+app.use(`${api}/orders`, orderRoutes)
 
 //Database
 mongoose
